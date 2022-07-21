0862ffa664ad8c1cf91fc7bd89163973d5ebf362
Braydon Wang <braydon.wang@gmail.com>
2022-07-20 22:53:55 -0400

    full-stack app with crud functionality

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eum culpabeatae incidunt quos, veritatis, eligendi voluptatibus dolorum libero fugiat, pariatur et cum optio Laudantium nostrum itaque quas labore perspiciatis',
+            'author': 'Braydon Wang',
+            'create_date': '04-25-2017',
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eum culpabeatae incidunt quos, veritatis, eligendi voluptatibus dolorum libero fugiat, pariatur et cum optio Laudantium nostrum itaque quas labore perspiciatis',
+            'author': 'Braydon Wang',
+            'create_date': '04-25-2017',
+        },
+        {
+            'id': 3, 
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eum culpabeatae incidunt quos, veritatis, eligendi voluptatibus dolorum libero fugiat, pariatur et cum optio Laudantium nostrum itaque quas labore perspiciatis',
+            'author': 'Braydon Wang',
+            'create_date': '04-25-2017',
+        }
+    ]
+    return articles
